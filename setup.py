81396c2610488a73a8930b2d20534d82e863d50e
Emre Erkunt <emre.erkunt@gmail.com>
2018-06-18 21:27:19 +0100

    Fixed Issue #5

---
+++ setup.py
@@ -0,0 +1,53 @@
+"""
+BDD test framework for terraform
+"""
+from setuptools import find_packages, setup
+from terraform_compliance.main import __app_name__, __version__
+
+dependencies = [
+    'radish-bdd',
+    'radish',
+    'terraform-validate'
+]
+
+setup(
+    name=__app_name__,
+    version=__version__,
+    url='https://github.com/eerkunt/terraform-compliance',
+    license='MIT',
+    author='Emre Erkunt',
+    author_email='emre.erkunt@gmail.com',
+    description='BDD test framework for terraform',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'terraform-compliance=terraform_compliance.main:cli',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
