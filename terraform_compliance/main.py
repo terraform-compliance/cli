a4c1d962f08ea8c01b09d43858ca51aefe9497fc
Emre Erkunt <emre.erkunt@gmail.com>
2018-09-09 14:19:57 +0100

    Moved ReadableDir to another file for beautification.

--- terraform_compliance/main.py
+++ terraform_compliance/main.py
@@ -1,12 +1,12 @@
-import sys
 import os
-from argparse import ArgumentParser, Action
+from argparse import ArgumentParser
 from radish.main import main as call_radish
 from tempfile import mkdtemp
 from git import Repo
 from terraform_compliance.common.pyhcl_helper import load_tf_files
 from distutils.dir_util import copy_tree
 from shutil import rmtree
+from terraform_compliance.common.readable_dir import ReadableDir
 
 
 __app_name__ = "terraform-compliance"
@@ -18,38 +18,6 @@ class ArgHandling(object):
 
 #TODO: Handle all directory/protocol handling via a better class structure here.
 #TODO: Extend git: (on features or tf files argument) into native URLs instead of using a prefix here.
-
-class ReadableDir(Action):
-    def __init__(self, dest, required, help, option_strings=None, metavar=None):
-        super(ReadableDir, self).__init__(dest, required, help, option_strings, metavar)
-        self.dest = dest
-        self.required = required
-        self.help = help
-        self.option_strings = option_strings
-        self.metavar = metavar
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        prospective_dir = values
-
-        # Check if the given directory is actually a git repo
-        if prospective_dir.startswith('git:'):
-            print('Using remote git repository: {}'.format(prospective_dir[4:]))
-            setattr(namespace, self.dest, prospective_dir[4:])
-            return True
-
-        # Check if the given path is a directory really
-        if not os.path.isdir(prospective_dir):
-            print('ERROR: {} is not a directory.'.format(prospective_dir))
-            sys.exit(1)
-
-        # Check if we have access to that directory
-        if os.access(prospective_dir, os.R_OK):
-            setattr(namespace, self.dest, prospective_dir)
-            return True
-
-        print('ERROR: Can not read {}'.format(prospective_dir))
-        sys.exit(1)
-
 
 def cli():
     argument = ArgHandling()
