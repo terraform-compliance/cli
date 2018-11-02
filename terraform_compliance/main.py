f3eff30e4d83787156b19e6be25241b67b60d2d5
Emre Erkunt <emre.erkunt@gmail.com>
2018-11-02 14:11:47 +0000

    Added more unit tests for increasing the coverage.

--- terraform_compliance/main.py
+++ terraform_compliance/main.py
@@ -38,7 +38,7 @@ from terraform_compliance.common.readable_dir import ReadableDir
 
 
 __app_name__ = "terraform-compliance"
-__version__ = "0.4.5"
+__version__ = "0.4.6"
 
 
 class ArgHandling(object):
