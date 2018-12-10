2308bc1ec4ac6334e8480e75dbfae8a55ee59ae3
Emre Erkunt <emre.erkunt@gmail.com>
2018-12-10 20:29:17 +0000

    Bumped up patch version by 1.

--- terraform_compliance/main.py
+++ terraform_compliance/main.py
@@ -39,7 +39,7 @@ from terraform_compliance.common.exceptions import TerraformComplianceInvalidCon
 
 
 __app_name__ = "terraform-compliance"
-__version__ = "0.4.9"
+__version__ = "0.4.10"
 
 
 class ArgHandling(object):
