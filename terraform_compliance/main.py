4b49ef89368a3b6d492a1e093a42d917182b7d5f
Demian Ginther <st.diluted@gmail.com>
2019-01-11 16:45:49 -0700

    Python 3 fixes to print statements

--- terraform_compliance/main.py
+++ terraform_compliance/main.py
@@ -9,7 +9,7 @@ except ImportError as e:
 
 
     def pip(action, package, params=None):
-        print '{}ing {}..'.format(action, package)
+        print('{}ing {}..'.format(action, package))
 
         if action == 'uninstall':
             cmds = [executable, "-m", "pip", action, '--yes', package]
@@ -18,15 +18,15 @@ except ImportError as e:
 
         subprocess.call(cmds)
 
-    print "Fixing the problem on radish and radish-bdd"
+    print("Fixing the problem on radish and radish-bdd")
     pip('uninstall', 'radish-bdd')
     pip('uninstall', 'radish')
     pip('install', 'radish==0.1.10')
     pip('install', 'radish-bdd==0.8.6')
 
-    print "~"*40
-    print " Please run terraform-compliance again."
-    print "~"*40
+    print("~"*40)
+    print(" Please run terraform-compliance again.")
+    print("~"*40)
     exit(1)
 
 from tempfile import mkdtemp
