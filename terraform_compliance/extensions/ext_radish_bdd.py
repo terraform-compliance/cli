e9a30a0a375e7de01fdf5c223303fa962d340a0e
Emre Erkunt <emre.erkunt@gmail.com>
2019-03-08 10:40:28 +0000

    Changed one of the console_write to print directly.

--- terraform_compliance/extensions/ext_radish_bdd.py
+++ terraform_compliance/extensions/ext_radish_bdd.py
@@ -50,4 +50,4 @@ def write_stdout(level, message):
     added_prefix = u'\n\t\t{}\t{} '.format(colorful.gray(u'\u2502'),' '*len(prefix))
     message = message.split('\n')
 
-    console_write('\t\t{}\t{} {}'.format(u'\u251c\u2501', prefix, added_prefix.join(message)))
+    print(u'\t\t\u251c\u2501\t{} {}'.format(prefix, added_prefix.join(message)))
