e1689400db68fbe447e366b335873abda724b8a3
vrb.cntrl <vrb.cntrl@LAPTOP-M9GBDQFH>
2019-07-16 12:37:45 -0400

    added new dict for encryption_at_rest and encryption_in_flight properties

--- terraform_compliance/steps/__init__.py
+++ terraform_compliance/steps/__init__.py
@@ -1,4 +1,4 @@
-encryption_property = {
+encryption_at_rest_property = {
     'aws_db_instance': 'storage_encrypted',
     'ebs_block_device': 'encrypted',
     'aws_ebs_volume': 'encrypted',
@@ -7,6 +7,10 @@ encryption_property = {
     'aws_s3_bucket': 'server_side_encryption_configuration',
     'aws_elasticache_replication_group': 'at_rest_encryption_enabled',
     'aws_emr_security_configuration': 'EnableAtRestEncryption'
+}
+encryption_in_flight_property = {
+    'aws_elasticache_replication_group': 'transit_encryption_enabled',
+    'aws_emr_security_configuration': 'EnableInTransitEncryption'
 }
 
 resource_name = {
@@ -25,5 +29,6 @@ resource_name = {
     'AWS VPC': 'aws_vpc',
     'Azure SQL Database': 'azurerm_sql_database',
     'Azure Storage Account': 'azurerm_storage_account',
-    'AWS Subnet': 'aws_subnet'
+    'AWS Subnet': 'aws_subnet',
+    'AWS ElastiCache Replication Group': 'aws_elasticache_replication_group'
 }
