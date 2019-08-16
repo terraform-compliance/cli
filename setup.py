84edb59f1c9d27757167915277f86be13ede2915
Emre Erkunt <emre.erkunt@gmail.com>
2019-08-16 11:15:39 +0100

    Bumped gitpython version to 3.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from terraform_compliance import __app_name__, __version__
 dependencies = [
     'radish>=0.1.10',
     'radish-bdd>=0.13.1',
-    'gitpython>=2.1.11',
+    'gitpython>=3.0.1',
     'netaddr>=0.7.19',
     'colorful>=0.5.1',
     'filetype>=1.0.5'
