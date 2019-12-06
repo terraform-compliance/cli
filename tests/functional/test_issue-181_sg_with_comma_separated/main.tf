12b8f0be64bc0791c11ee9049bdbd9f27970a210
Emre Erkunt <emre.erkunt@gmail.com>
2019-12-06 09:25:20 +0000

    Added Security Group related problematic integration tests

---
+++ tests/functional/test_issue-181_sg_with_comma_separated/main.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "allow_tls_ingress_inline" {
+  name        = "allow_tls_ingress_inline"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
