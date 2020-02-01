c63729962b3dfb1e46d207097e4e1ca9d23e5a55
Emre Erkunt <emre.erkunt@gmail.com>
2020-02-01 23:42:20 +0000

    Fixed dependencies within the python package.

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ from terraform_compliance import __app_name__, __version__
 
 dependencies = [
     'radish-bdd>=0.13.1',
-    'gitpython>=3.0.2',
+    'gitpython>=3.0.5',
     'netaddr>=0.7.19',
-    'colorful>=0.5.1',
+    'colorful>=0.5.4',
     'filetype>=1.0.5',
     'junit-xml>=1.8',
-    'lxml>=4.4.1'
+    'lxml>=4.5.0',
+    'emoji>=0.5.4'
 ]
 
 setup(
