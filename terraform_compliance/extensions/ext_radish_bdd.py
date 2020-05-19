210f404698dc969ea0d5c755047eda5de38b1f9c
Emre Erkunt <emre.erkunt@gmail.com>
2020-05-19 13:37:17 +0100

    Added one space tolerance to the PROPERTY regex for backward compatibility.

--- terraform_compliance/extensions/ext_radish_bdd.py
+++ terraform_compliance/extensions/ext_radish_bdd.py
@@ -49,7 +49,9 @@ def custom_type_any(text):
     return text.replace('"', '').replace('\'', '')
 
 
-@custom_type("PROPERTY", r"(\"[\s\*\.\/_\-A-Za-z0-9:\(\)\[\]\']+\")|([\*\.\/_\-A-Za-z0-9:\(\)\[\]\']+)")
+@custom_type("PROPERTY", r"([\*\.\/_\-A-Za-z0-9:\(\)\[\]\']+\s[\*\.\/_\-A-Za-z0-9:\(\)\[\]\']+)|"
+                         r"(\"[\s\*\.\/_\-A-Za-z0-9:\(\)\[\]\']+\")|"
+                         r"([\*\.\/_\-A-Za-z0-9:\(\)\[\]\']+)")
 def custom_type_prop(text):
     return text.replace('"', '').replace('\'', '')
 
