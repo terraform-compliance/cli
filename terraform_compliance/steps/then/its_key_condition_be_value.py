03b9e805e7a8a5e4bdef7807d3336725a01ec923
Kaan Katircioglu <kaankatircioglu@gmail.com>
2020-06-15 23:14:56 +0300

    fix its_key_condition_be_value

--- terraform_compliance/steps/then/its_key_condition_be_value.py
+++ terraform_compliance/steps/then/its_key_condition_be_value.py
@@ -11,6 +11,7 @@ from terraform_compliance.common.exceptions import TerraformComplianceNotImpleme
 def its_key_condition_be_value(_step_obj, key, condition, value, stash=Null, depth=0):
     if condition not in ('must', 'must not'):
         raise TerraformComplianceNotImplemented('This step only accepts "must" and "must not" as a condition.')
+    condition = condition == 'must'
 
     if stash is Null:
         stash = _step_obj.context.stash
@@ -19,32 +20,47 @@ def its_key_condition_be_value(_step_obj, key, condition, value, stash=Null, dep
         Error(_step_obj, 'No entities found for this step to process. Check your filtering steps in this scenario.')
         return False
 
-    found_values = []
-    for entity in stash:
-        if isinstance(entity, dict):
-            found_values.extend(seek_regex_key_in_dict_values(entity, key, value))
-        elif isinstance(entity, list):
-            for element in entity:
-                found_values.extend(its_key_condition_be_value(_step_obj, key, condition, element, entity, depth+1))
-        elif isinstance(entity ,(str, int, bool)) and (str(entity).lower == key.lower or str(entity) == value.lower):
-            found_values.append(entity)
-
-    # Return the values to the parent call.
-    if depth > 0:
-        return found_values
-
-    condition = condition == 'must'
-    found_values = [values for values in found_values if values is not None]
-
     obj_address = _step_obj.context.name
     if hasattr(_step_obj.context, 'address'):
         obj_address = _step_obj.context.address
     elif hasattr(_step_obj.context, 'addresses'):
         obj_address = ', '.join(_step_obj.context.addresses)
 
+    found_values = []
+    for entity in stash:
+        if isinstance(entity, dict):
+            found_value = seek_regex_key_in_dict_values(entity, key, value)
+            if not found_value and condition:
+                Error(_step_obj, 'Can not find {} in {} property of {}.'.format(value, key, entity.get('address', obj_address)))
+            elif found_value and not condition:
+                Error(_step_obj, 'Found {}({}) in {} property of {}.'.format(value, ', '.join(found_values), key, obj_address))
+
+            found_values.extend(found_value[:1]) # only extend by the first element
+
+        elif isinstance(entity, list):
+            dict_entity = {f'not_{str(key)}': entity}
+            found_value = seek_regex_key_in_dict_values(dict_entity, key, value)
+            if not found_value and condition:
+                Error(_step_obj, 'Can not find {} in {} property of {}.'.format(value, key, obj_address))
+            elif found_value and not condition:
+                Error(_step_obj, 'Found {}({}) in {} property of {}.'.format(value, ', '.join(found_values), key, obj_address))
+
+
+            found_values.extend(found_value[:1]) # only extend by the first element
+
+        elif isinstance(entity ,(str, int, bool)):
+            # raise error because you don't have a {key: value}
+            if (str(entity).lower == key.lower or str(entity) == value.lower) and condition:
+                Error(_step_obj, 'Value {} found in {} property of {}, but is not in {{key: value}} format.'.format(value, key, obj_address))
+            if condition:
+                Error(_step_obj, 'Can not find {} in {} property of {}.'.format(value, key, obj_address))
+
+
+    _step_obj.context.stash = found_values
+
     if found_values and not condition:
         Error(_step_obj, 'Found {}({}) in {} property of {}.'.format(value, ', '.join(found_values), key, obj_address))
-    elif not found_values and condition:
+    elif len(found_values) != len(stash) and condition:
         Error(_step_obj, 'Can not find {} in {} property of {}.'.format(value, key, obj_address))
 
     return True
