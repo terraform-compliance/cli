ba62d5e15d6f0d192dfd03b22d2f0f464c85b61f
Kaan Katircioglu <kaankatircioglu@gmail.com>
2020-07-10 17:56:11 +0300

    Refactor to use world.config.user_data instead of global variables

--- terraform_compliance/steps/terrain.py
+++ terraform_compliance/steps/terrain.py
@@ -1,9 +1,7 @@
-import curses
 import json
 from IPython import embed
 from radish import before, after, world
 from terraform_compliance.extensions.terraform import TerraformParser
-from terraform_compliance.main import cli_arguments
 
 @before.each_feature
 def load_terraform_data(feature):
@@ -12,9 +10,7 @@ def load_terraform_data(feature):
 
 @after.each_step
 def wait_for_user_input(step):
-    global cli_arguments
-
-    if not cli_arguments.debug:
+    if world.config.user_data['debugging_mode_enabled'] == 'False':
         return
 
     cmd = 'cmd'
