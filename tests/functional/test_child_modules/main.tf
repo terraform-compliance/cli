c6de57afc340635bdfba134466332ab9e2b8b171
Prazzy <prazzee@gmail.com>
2021-06-04 06:10:49 -0400

    Fixing a problem while reading the plan (#494) (#497)
    * Fixing a problem while reading the plan (#494)
    
    If a state file contains "child_modules", below error is thrown.
    So this commit will handle the case of supporting child modules.
    
    ERROR: state.out.json is not a valid terraform plan json output.
    
    * Update terraform_compliance/common/readable_plan.py
    
    Co-authored-by: Emre Erkunt <emre.erkunt@gmail.com>
    
    * Update tests/functional/test_child_modules/main.tf
    
    Co-authored-by: Emre Erkunt <emre.erkunt@gmail.com>
    
    Co-authored-by: Praveen Shilavantar <praveenshilavantar@Praveens-MacBook-Pro.local>
    Co-authored-by: Emre Erkunt <emre.erkunt@gmail.com>

---
+++ tests/functional/test_child_modules/main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "fail" {
+  bucket = "some_random_bucket_name-123808h0i1h3085018g531"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "123456"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
