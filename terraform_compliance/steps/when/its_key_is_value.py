97da0df22a3b7c62de293239e24742e8f2561940
Emre Erkunt <emre.erkunt@gmail.com>
2022-03-22 11:26:57 +0000

    Added regex support for When its key is value

--- terraform_compliance/steps/when/its_key_is_value.py
+++ terraform_compliance/steps/when/its_key_is_value.py
@@ -8,8 +8,9 @@ from terraform_compliance.common.helper import (
 from terraform_compliance.extensions.ext_radish_bdd import skip_step
 
 
-def its_key_is_value(_step_obj, key, value, dict_value=None, address=Null):
+def its_key_is_value(_step_obj, key, value, dict_value=None, address=Null, regex_match=Null):
     match = _step_obj.context.match
+    match.regex_flag = regex_match
     
     orig_key = key
     if key == 'reference':
@@ -76,8 +77,9 @@ def its_key_is_value(_step_obj, key, value, dict_value=None, address=Null):
                                                                                ', '.join(_step_obj.context.addresses)))
 
 
-def its_key_is_not_value(_step_obj, key, value, dict_value=None, address=Null):
+def its_key_is_not_value(_step_obj, key, value, dict_value=None, address=Null, regex_flag=Null):
     match = _step_obj.context.match
+    match.regex_flag = regex_flag
     
     orig_key = key
     if key == 'reference':
